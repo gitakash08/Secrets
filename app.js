93d2b62ad15d0f86171fb20288b9ef4e6e5ef206
Akash <tripathiakash387@gmail.com>
2020-12-21 12:32:37 +0530

    environment files

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require('mongoose-encryption');
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine' ,'ejs');
@@ -16,9 +19,8 @@ const userSchema = new mongoose.Schema({
   password:String
 });
 
-const secret = "Thisisournewsecret.";
 
-userSchema.plugin(encrypt, { secret: secret , encryptedFields: ['password']});
+userSchema.plugin(encrypt, { secret:process.env.SECRET , encryptedFields: ['password']});
 
 const User = new mongoose.model("User" , userSchema);
 
